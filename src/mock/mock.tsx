bd1eaeaf6178455f4f99de48ba7d98e2dde3a043
dashixiong-11 <2788347834@qq.com>
2022-10-09 18:35:20 +0800

    自制 mock

---
+++ src/mock/mock.tsx
@@ -0,0 +1,8 @@
+import { faker } from '@faker-js/faker'
+import { AxiosRequestConfig } from 'axios';
+
+type Mock = (config:AxiosRequestConfig) => [number, any]
+
+faker.setLocale('zh_CN')  
+
+export const mockSession: Mock = (config) => [200, { jwt: faker.random.word() }]
