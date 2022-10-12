683b95307bedfee9d27c583113290b819b25bdfe
dashixiong-11 <2788347834@qq.com>
2022-10-12 22:12:12 +0800

    分页加载

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -8,11 +8,17 @@ faker.setLocale('zh_CN')
 export const mockSession: Mock = (config) => [200, { jwt: faker.random.word() }]
 
 export const mockTagIndex: Mock = (config) => {
+  const { kind, page } = config.params
+  const per_page = 25
+  const count = 26
   let id = 0
   const createId = () => {
     id += 1
     return id
   }
+  const createPaper = (page = 1) => ({
+    page, per_page, count
+  }) 
   const createTag = (n = 1, attrs?: any) =>
     Array.from({ length: n }).map(() => ({
       id: createId(),
@@ -21,11 +27,15 @@ export const mockTagIndex: Mock = (config) => {
       kind: config.params.kind,
       ...attrs
     }))
-
-  if (config.params.kind === 'expenses') {
-    return [200, { resources: createTag(7) }]
-  } else {
-    return [200, { resources: createTag(20) }]
-  }
+    const createBody = (n = 1, attrs?: any) => ({
+      resources: createTag(n), pager: createPaper(page)
+    })
+    if (kind === 'expenses' && (page === 1 || !page)) {
+      return [200, createBody(25)]
+    } else if (kind === 'expenses' && page === 2) {
+      return [200, createBody(1)]
+    } else {
+      return [200, { resources: createTag(20) }]
+    }
 
 }
