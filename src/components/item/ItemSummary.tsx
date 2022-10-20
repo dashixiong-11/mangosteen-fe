6b634438a0c317df49fbe3b656dcd7655bb64b02
dashixiong-11 <2788347834@qq.com>
2022-10-20 16:05:31 +0800

    封装 datetiem  和 money

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,7 +1,9 @@
 import { defineComponent, onMounted, PropType, ref } from 'vue'
 import { Button } from '../../shared/Button'
+import { Datetime } from '../../shared/Datetime'
 import { FloatButton } from '../../shared/FloatButton'
 import { http } from '../../shared/Http'
+import { Money } from '../../shared/Money'
 import s from './ItemSummary.module.scss'
 export const ItemSummary = defineComponent({
   props: {
@@ -59,9 +61,9 @@ export const ItemSummary = defineComponent({
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
                       <span class={s.tag}>{item.tags_id[0]}</span>
-                      <span class={s.amount}>￥<>{item.amount}</></span>
+                      <span class={s.amount}>￥<Money value={item.amount}/></span>
                     </div>
-                    <div class={s.time}>{item.happen_at}</div>
+                    <div class={s.time}><Datetime value={item.happen_at}/></div>
                   </div>
                 </li>
               ))}
