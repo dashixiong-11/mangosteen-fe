8736198d4a883e8dc71ba638b96827ae91c66585
dashixiong-11 <2788347834@qq.com>
2022-10-22 10:11:30 +0000

    kind 切换请求

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue';
+import { computed, defineComponent, onMounted, PropType, ref, watch } from 'vue'
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 import { LineChart } from './LineChart';
@@ -41,8 +41,8 @@ export const Charts = defineComponent({
       }) 
     })
 
-    onMounted(async ()=>{
-      const response = await http.get<{groups: Data1, summary: number}>('/items/summary',{
+    const fetchData1 = async () => {
+      const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
@@ -50,7 +50,10 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
-    })
+    }
+
+    onMounted(fetchData1)
+    watch(() => kind.value, fetchData1)
 
     const data2 = ref<Data2>([])
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
@@ -60,7 +63,7 @@ export const Charts = defineComponent({
       }))
     )
 
-    onMounted(async () => {
+    const fetchData2 = async ()=>{
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -69,7 +72,10 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data2.value = response.data.groups
-    })
+    }
+
+    onMounted(fetchData2)
+    watch(() => kind.value, fetchData2)
 
     const betterData3 = computed<{tag:Tag, amount:number, percent: number}[]>(()=>{
       const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
