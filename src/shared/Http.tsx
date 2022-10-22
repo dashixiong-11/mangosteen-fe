50dcc819df5b79ad92ab2a2ffdfa646370d528cc
dashixiong-11 <2788347834@qq.com>
2022-10-22 09:00:21 +0000

    折线图 数据转换

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,6 +1,5 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { mockItemCreate, mockItemIndex, mockItemIndexBalance, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
-
+import { mockItemCreate, mockItemIndex, mockItemIndexBalance, mockItemSummary, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
 
 
 
@@ -66,6 +65,9 @@ const mock = (response: AxiosResponse) => {
       return true
     case 'itemIndexBalance':
       [response.status, response.data] = mockItemIndexBalance(response.config)
+      return true
+    case 'itemSummary':
+      [response.status, response.data] = mockItemSummary(response.config)
       return true
   }
   return false
